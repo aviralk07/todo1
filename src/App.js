4358d7045b2ea7b3b1b12d52d11c138ac4a1e488
aviral kalra <aviral.kalrafullthrottlelabs.com>
2023-10-24 22:15:02 +0530

    aaa

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  const [task, setTask] = useState("");
+  const [editIndex, setEditIndex] = useState(-1);
+
+  useEffect(() => {
+    // Load tasks from local storage when the component first renders
+    const storedTasks = JSON.parse(localStorage.getItem("tasks"));
+    console.log("Stored Tasks:", storedTasks);
+    if (storedTasks) {
+      setTasks(storedTasks);
+    }
+  }, []);
+
+  const addTask = () => {
+    if (task.trim() !== "") {
+      if (editIndex === -1) {
+        setTasks([...tasks, task]);
+      } else {
+        const newTasks = [...tasks];
+        newTasks[editIndex] = task;
+        setTasks(newTasks);
+        setEditIndex(-1);
+      }
+      setTask("");
+    }
+  };
+
+  const removeTask = (index) => {
+    const newTasks = [...tasks];
+    newTasks.splice(index, 1);
+    setTasks(newTasks);
+  };
+
+  const editTask = (index) => {
+    setEditIndex(index);
+    setTask(tasks[index]);
+  };
+
+  useEffect(() => {
+    // Save tasks to local storage whenever tasks change
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]); // This effect runs whenever tasks change
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>To-Do List</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="Add or Edit a task"
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+        />
+        <button onClick={addTask}>{editIndex === -1 ? "Add" : "Edit"}</button>
+      </div>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            <span>{task}</span>
+            <div>
+              <button className="edit-button" onClick={() => editTask(index)}>
+                Edit
+              </button>
+              <button
+                className="delete-button"
+                onClick={() => removeTask(index)}
+              >
+                Delete
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
